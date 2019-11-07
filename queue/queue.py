5cda2e14227aba3bfae37119f9954f24848385d9
cecil john tantay <cjbtantay@gmail.com>
2019-11-06 23:15:55 -0800

    add doubly linked list as storage

--- queue/queue.py
+++ queue/queue.py
@@ -1,15 +1,18 @@
+import doubly_linked_list
+
+
 class Queue:
-  def __init__(self):
-    self.size = 0
-    # what data structure should we
-    # use to store queue elements?
-    self.storage = 
+    def __init__(self):
+        self.size = 0
+        # what data structure should we
+        # use to store queue elements?
+        self.storage = doubly_linked_list
 
-  def enqueue(self, item):
-    pass
-  
-  def dequeue(self):
-    pass
+    def enqueue(self, item):
+        pass
 
-  def len(self):
-    pass
+    def dequeue(self):
+        pass
+
+    def len(self):
+        pass
